2f65037788a4b1cafc24c69c01843d245c6faa77
Thomas Koopman <koopman.t.pro@gmail.com>
2026-08-26 21:16:04 +0200

    Revert "Set short timeout for testing"
    This reverts commit 4118c93511736bf6072b7d61e375378ca0a0b1bc.

--- sagemaker_shim/models.py
+++ sagemaker_shim/models.py
@@ -1238,8 +1238,7 @@ class InferenceTask(BaseModel):
             try:
                 return_code = await asyncio.wait_for(
                     user_process.run_inference(task=self),
-                    timeout=3,
-                    # timeout=self.timeout.total_seconds(),
+                    timeout=self.timeout.total_seconds(),
                 )
             except TimeoutError:
                 user_process.mark_unhealthy()
